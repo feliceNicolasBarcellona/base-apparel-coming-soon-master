85427f838fb25dd6370b29747fa57bfd43fc0e35
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2023-02-19 16:56:08 +0100

    final commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const textError = document.querySelector(".error");
+const iconError = document.querySelector(".icon-error");
+
+const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+form.addEventListener('click', () => {
+  if(email.value !== '' && !emailRegExp.test(email.value)){
+    iconError.style.display = 'block';
+    textError.innerHTML = 'Please provide a valid email';
+    email.className = 'invalid';
+  } else {
+    iconError.style.display = 'none';
+    textError.innerHTML = '';
+    email.className = 'valid';
+  }
+})
